0261283f0a4b7b27ac4b5bd4d73d17b26b7cb6a4
petitengineer <jonny.owuwe@simplelogin.co>
2022-05-18 14:43:39 -0400

    Added basic project files, models, and database schemes.

--- README.txt
+++ README.txt
@@ -2,8 +2,10 @@ This is the backend for an inventory tracking web application. It was created sp
 
 It is also my first ever web application. I used it to help myself better understand Python & Django, SQL, and concepts behind web development.
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~HOW TO USE THIS WEB APPLICATION~~~~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## HOW TO USE THIS WEB APPLICATION
 
 This section has been intentionally left blank because I have not yet completed the application. I will update it later.
+
+# NOTES FOR SELF
+
---
+++ warehouse/models.py
@@ -0,0 +1,29 @@
+from pydoc import describe
+from django.db import models
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(default="-")
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits = 6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now = True) #indicates date-time of last edit
+    shelving_unit =  models.CharField(max_length=3)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=255)
+    square_footage = models.DecimalField(max_digits=8, decimal_places=5)
+    
+
+class Address(models.Model):
+    street_address = models.TextField(max_length=255)
+    zip_code = models.TextField(max_length=10)
+    unit_number = models.IntegerField()
+    city_name = models.TextField(max_length=255)
+    country_or_province = models.TextField(max_length=255)
+    country = models.TextField(max_length=255)
+    warehouse = models.OneToOneField(Warehouse, on_delete=models.CASCADE, primary_key=True)
