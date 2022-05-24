6823b3c5fb70b81346ef51b8df597fd1498778af
petitengineer <jonny.owuwe@simplelogin.co>
2022-05-24 18:52:17 -0400

    Cleaned up views code for better readability.

--- warehouse/views.py
+++ warehouse/views.py
@@ -9,37 +9,30 @@ import json #from python
 @csrf_exempt
 def list_products(request):
     if request.method == 'GET': #View
-        print("it was a get")
-        print(request.body)
         product_queryset = Product.objects.all()
         warehouse_queryset = Warehouse.objects.all()
-
         return render(request, 'list_everything.html', {'products': list(product_queryset), 'warehouses': list(warehouse_queryset)})
 
-        ##################################
     elif request.method == 'POST': #Create
         data = request.body
         json_data = json.loads(data.decode("utf-8").replace("'",'"'))
-        print("~~~~The dictionary: ", json_data)
         create(json_data)
+        return HttpResponse("Successfully added the following to the database: \n" + str(json_data))
 
-        print("it was a post")
     elif request.method == 'PUT': #Update
-        print("Here's your data \n")
         data = request.body
         json_data = json.loads(data.decode("utf-8").replace("'",'"'))
         update(json_data)
-        print("it was a put")
+        return HttpResponse("Successfully modified the following in the database: \n" + str(json_data))
+
     elif request.method == 'DELETE': #Delete
         data = request.body
         json_data = json.loads(data.decode("utf-8").replace("'",'"'))
         delete(json_data)
-        print("successfully deleted")
-    else:
-        print("ERROR: COMMAND NOT RECOGNIZED.")
+        return HttpResponse("Successfully modified the following in the database: \n" + str(json_data))
 
-    
-    return HttpResponse("Command executed successfully!\n")
+    else:
+        return HttpResponse("ERROR: COMMAND NOT RECOGNIZED! \n")
 
 def create(data):
     try:
@@ -48,20 +41,18 @@ def create(data):
             Warehouse.objects.create(**data)
         elif data_type == 'Product':
             Product.objects.create(**data)
-            print('product was created!')
     except:
-            print('Data was mislabled or invalid.')
+        print('Data was mislabled or invalid.')
 
 def update(data):
     try:
         data_type = data.pop('type')
         if data_type == 'Warehouse':
-            print("updating warehouse data")
             Warehouse.objects.filter(pk=data.pop('id')).update(**data)            
         elif data_type == 'Product':
             Product.objects.filter(pk=data.pop('id')).update(**data)
     except:
-            print('Data was mislabled or invalid.')
+        print('Data was mislabled or invalid.')
 
 def delete(data):
     try:
