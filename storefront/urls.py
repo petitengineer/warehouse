3101e69dc1cb660fe47ef5a2da71bb1ca756bf32
petitengineer <jonny.owuwe@simplelogin.co>
2022-05-23 21:18:43 -0400

    Added the ability to view a list of inventory items and warehouses.

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('warehouse/', include('warehouse.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
--- warehouse/views.py
+++ warehouse/views.py
@@ -1,3 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from warehouse.models import Warehouse, Product
 
 # Create your views here.
+def list_products(request):
+    product_queryset = Product.objects.all()
+    warehouse_queryset = Warehouse.objects.all()
+
+    return render(request, 'list_everything.html', {'products': list(product_queryset), 'warehouses': list(warehouse_queryset)})
+    #return HttpResponse("Hello World!")
